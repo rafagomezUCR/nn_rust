5b86d86776abbb86ce93faaa57b5b77ab797e946
rafagomezUCR <rgome020@ucr.edu>
2025-12-03 13:32:35 -0800

    added test for all activations in Activations.rs

--- src/activations.rs
+++ src/activations.rs
@@ -47,8 +47,64 @@ mod tests {
 
     #[test]
     fn sigmoid_f_test() {
-        let expected = 1.0 / (1.0 + (-1.0f64).exp());
-        assert!( (Activations::Sigmoid.f(0.0) - 0.5).abs() < THRESHOLD);
-        assert!( (Activations::Sigmoid.f(1.0) - expected).abs() < THRESHOLD)
+        let values: Vec<f64> = vec![-1.0, 0.0, 1.0, 2.0];
+        for &v in values.iter() {
+            let expected = 1.0 / (1.0 + (-v).exp());
+            let actual = Activations::Sigmoid.f(v);
+            assert!( (actual - expected).abs() < THRESHOLD);
+        }
+    }
+
+    #[test]
+    fn sigmoid_df_test() {
+        let values: Vec<f64> = vec![-1.0, 0.0, 1.0, 2.0];
+        for &v in values.iter() {
+            let s = 1.0 / (1.0 + (-v).exp());
+            let expected = s * (1.0 - s);
+            let actual = Activations::Sigmoid.df(v);
+            assert!( (actual - expected).abs() < THRESHOLD);
+        }
+    }
+
+    #[test]
+    fn tanh_f_test() {
+        let values: Vec<f64> = vec![-1.0, 0.0, 1.0, 2.0];
+        for &v in values.iter() {
+            let expected = v.tanh();
+            let actual = Activations::Tanh.f(v);
+            assert!( (actual - expected).abs() < THRESHOLD);
+        }
+    }
+
+    #[test]
+    fn tanh_df_test() {
+        let values: Vec<f64> = vec![-1.0, 0.0, 1.0, 2.0];
+        for &v in values.iter() {
+            let expected = 1.0 - v.tanh() * v.tanh();
+            let actual = Activations::Tanh.df(v);
+            assert!( (actual - expected).abs() < THRESHOLD);
+        }
+    }
+
+    #[test]
+    fn relu_f_test() {
+        let values: Vec<f64> = vec![-1.0, 0.0, 1.0, 2.0];
+        for &v in values.iter() {
+            let expected = v.max(0.0);
+            let actual = Activations::Relu.f(v);
+            assert!( (actual - expected).abs() < THRESHOLD);
+        }
+    }
+
+    #[test]
+    fn relu_df_test() {
+        let values: Vec<f64> = vec![-1.0, 0.0, 1.0, 2.0];
+        for &v in values.iter() {
+            let expected = {
+                if v <= 0.0 { 0.0 } else { 1.0 }
+            };
+            let actual = Activations::Relu.df(v);
+            assert!( (actual - expected).abs() < THRESHOLD);
+        }
     }
 }
